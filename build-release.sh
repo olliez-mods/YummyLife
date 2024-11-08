ed3960fd9b2c0c42d14af4a62043dc590267b1e2
olliez-mods <socksinthewash@gmail.com>
2024-11-08 15:59:04 -0500

    Change GitHub workflow to point to YummyLife and change EXE name

--- build-release.sh
+++ build-release.sh
@@ -12,14 +12,14 @@ echo ----- Linux -----
 cmake -B relbuild/linux -S .
 cmake --build relbuild/linux -j
 
-mv relbuild/linux/YumLife_linux relbuild/
-mv relbuild/windows/YumLife_windows.exe relbuild/
+mv relbuild/linux/YummyLife_linux relbuild/
+mv relbuild/windows/YummyLife_windows.exe relbuild/
 
 for dst in ahap ohol; do
   if [ -e ~/$dst ]; then
     echo Copying to ~/$dst
-    cp relbuild/YumLife_linux ~/$dst/
-    cp relbuild/YumLife_windows.exe ~/$dst/
+    cp relbuild/YummyLife_linux ~/$dst/
+    cp relbuild/YummyLife_windows.exe ~/$dst/
   fi
 done
 
