be00cd299e2b4233a6a5f4c3c918f91faf41730a
olliez-mods <socksinthewash@gmail.com>
2025-01-19 01:49:09 -0500

    Switch from curl to cpp-httplib and OpenSSL. Took way too much time. Use precompiled openssl libs from olliez/OpenSSL-Prebuilt

--- yummyLifeBuildScripts/setup-libs.sh
+++ yummyLifeBuildScripts/setup-libs.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
+set -e
 
 # This script expects to be run from the 'yummyLifeBuildScripts/' directory
 cd ../
 
 # Clean up old files
-echo --- CLEANUP ---
+echo ----- CLEANUP -----
 echo Removing existing folder/files
 rm -rf ./SDL-devel-1.2.15-mingw32.tar.gz
 rm -rf ./SDL-1.2.15
 rm -rf ./._SDL-1.2.15
-rm -rf ./curl-7.79.1.tar.gz
-rm -rf ./libcurl-source
-rm -rf ./curl-7.79.1
-rm -rf ./libcurl-7.79.1
-echo ---------------
+rm -rf ./CPP-HTTPLib
+rm -rf ./openssl-3.0.15-i686.tar.gz
+rm -rf ./openssl-3.0.15-i686
+rm -rf ./openssl-3.0.15
+echo -------------------
 echo
 
 # SDL - Simply download and extract
-echo ----- SDL -----
-curl -O https://www.libsdl.org/release/SDL-devel-1.2.15-mingw32.tar.gz
+echo ------- SDL -------
+wget https://www.libsdl.org/release/SDL-devel-1.2.15-mingw32.tar.gz
 tar zxvf SDL-devel-1.2.15-mingw32.tar.gz
 rm SDL-devel-1.2.15-mingw32.tar.gz
-echo ---------------
+echo -------------------
 echo
 
-# Skip LibCurl for now
-exit 0
-# LibCurl - Needs to be built on the system
-echo --- LIBCURL ---
-curl -O https://curl.se/download/curl-7.79.1.tar.gz
-tar zxvf curl-7.79.1.tar.gz
-rm curl-7.79.1.tar.gz
-mkdir -p ./libcurl-7.79.1
-cd curl-7.79.1
-./configure --prefix=$(pwd)/../libcurl-7.79.1 --with-ssl
-make && make install
-cd ..
-rm -rf curl-7.79.1
-echo ---------------
+# Cpp-httplib - Simply download the header file, it's all in one ;D
+echo --- CPP-HTTPLIB ---
+mkdir -p ./CPP-HTTPLib
+wget -O ./CPP-HTTPLib/httplib.h https://raw.githubusercontent.com/yhirose/cpp-httplib/master/httplib.h
+echo -------------------
+echo
+
+# Download precompiled OpenSSL (32-bit) from olliez-mods/openssl-prebuilt
+# Building them yourself takes a long time, so this is faster
+echo ----- OPENSSL -----
+wget https://github.com/olliez-mods/OpenSSL-Prebuilt/releases/download/3.0.15-i686/openssl-3.0.15-i686.tar.gz
+tar zxvf openssl-3.0.15-i686.tar.gz
+rm openssl-3.0.15-i686.tar.gz
+cp -rf openssl-3.0.15-i686 openssl-3.0.15
+rm -rf openssl-3.0.15-i686
+echo -------------------
 echo
